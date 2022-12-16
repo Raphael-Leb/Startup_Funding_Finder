4f058fe25807a80c27d5e98f0a692d12184d33e4
Raphael-Leb <62116334+Raphael-Leb@users.noreply.github.com>
2022-12-16 13:54:00 -0500

    Added export to excel button and made it so app can display empty list

--- main.py
+++ main.py
@@ -13,42 +13,55 @@ collection = db["Sources"]
 st.title("Funding Sources for Startups")
 st.text("This app displays information about different funding sources for startups.")
 
-funding_sources = collection.find({},projection={"_id": False,"name": 1, "type": 1, "amount":1 , "city": 1, "requirements": 1, "contact": 1})
+#sources_found = 0
+funding_sources = collection.find({},projection=table_info)
 
+# Button to allow the user to show all funding sources
+if st.button("Show all funding sources"):
+    # Retrieve data from the database and display it in the app
+    funding_sources = collection.find({},projection=table_info)
+    #sources_found = 1
+
+# Filter the data by type
 with st.expander("Show funding sources by type"):
     # Add input widgets to allow the user to control the app
-    funding_type = st.selectbox("Select funding type:", ["seed", "venture", "angel", "bursary", "grant", "loan", "equity", "other"])
+    funding_type = st.selectbox("Select funding type:", funding_types)
     if st.button("Show funding sources"):
-        # Retrieve data from the database and display it in the app
-        funding_sources = collection.find({"type": funding_type},projection={"_id": False,"name": 1, "type": 1,"amount":1 , "city": 1, "requirements": 1, "contact": 1})
-        
+        funding_sources = collection.find({"type": funding_type},projection=table_info)
 
+# Filter the data by city
 with st.expander("Show funding sources by city"):
     # Add input widgets to allow the user to control the app
     city = st.selectbox("Select city:", list(collection.distinct("city")))
     if st.button("Show funding sources by city"):
-        # Retrieve data from the database and display it in the app
-        funding_sources = collection.find({"city": city},projection={"_id": False,"name": 1, "type": 1, "amount":1 ,"city": 1, "requirements": 1, "contact": 1})
+        funding_sources = collection.find({"city": city},projection=table_info)
 
+funding_sources_export = funding_sources.clone()
 display_info_table(funding_sources)
 
-#st.write("Export this list to excel:") # I cannot get this to work 
-#if st.button("Export to excel"):
-    # Export the data to an excel file
-    #outfile = open('output.csv', 'wb')
-    #df = pd.DataFrame(dtype=int)
-    #df.to_csv(outfile, index=False)
-    #outfile.close()
-    #download the excel file
-    #st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+col1, col2 = st.columns([1,3.3])
+with col1:
+    st.write("Export this list to excel:") 
+
+# Export the data to an excel file
+outfile = open('output.csv', 'wb')
+df = pd.DataFrame(list(funding_sources_export))
+df.to_csv(outfile, index=False)
+outfile.close()
+
+with col2:
+    with open("output.csv", "rb") as file:
+        btn = st.download_button(label='Download', data=file, file_name='funding_sources.csv', mime='text/csv')
+
 
 st.title("Contribute to the database")
 with st.expander("Add a new funding source"):
     # Add input widgets for adding new funding source data
     st.title("Add a new funding source")
     name = st.text_input("Name")
-    type = st.selectbox("Type", ["seed", "venture", "angel", "bursary", "grant", "loan", "equity", "other"])
+    type = st.selectbox("Type", funding_types)
     city = st.selectbox("City", list(collection.distinct("city")))
+    city = st.text_input("City")
     amount = st.number_input("Amount", step=1000)
     requirements = st.text_area("requirements")
     contact = st.text_input("Contact")
@@ -69,9 +82,9 @@ with st.expander("Add a new funding source"):
 with st.expander("Edit funding source data"):
     # Add input widgets for editing funding source data
     name = st.text_input("Name of funding source to edit")
-    new_type = st.selectbox("New type ", ["seed", "venture", "angel", "bursary", "grant", "loan", "equity", "other"])
+    new_type = st.selectbox("New type ", funding_types)
     new_city = st.selectbox("New city ", list(collection.distinct("city")))
-    new_amount = st.number_input("New amount ")
+    new_amount = st.number_input("New amount ",step=1000)
     new_requirements = st.text_area("New requirements ")
     new_contact = st.text_input("New contact ")
 
@@ -79,3 +92,7 @@ with st.expander("Edit funding source data"):
     if st.button("Edit funding source"):
         # Use the edit_funding_source() function to update the data in the database
         edit_funding_source(collection, name, new_type, new_city, new_amount, new_requirements, new_contact)
+
+st.write("")
+st.write("Made by Raphael Leblanc, any contributions are welcome!")
+st.write("Github: https://short.raltech.us/r/3pvdW")
